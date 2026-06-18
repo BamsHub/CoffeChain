dd8a20f6788abbb521aae7219a4cd5e9d3d080bd
BamsHub <153614329+BamsHub@users.noreply.github.com>
2026-06-18 08:42:01 +0700

    use Solana testnet for blockchain testing

--- contracts/programs/coffee-payment/src/lib.rs
+++ contracts/programs/coffee-payment/src/lib.rs
@@ -5,7 +5,7 @@ declare_id!("CvtD1ar7TCaw69xdZYTPPb2vfNQBLnofbmdfHN6HwX52");
 
 /// =============================================================
 ///  CoffeeChain — Smart Contract Pembayaran Kopi
-///  Network: Solana Devnet / Mainnet
+///  Network: Solana Testnet / Mainnet
 ///  Gas: ~5000 lamports (< Rp 1) — paling hemat dengan Compute Budget
 /// =============================================================
 #[program]
